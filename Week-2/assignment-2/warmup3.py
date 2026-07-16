ac024d50e773199c3f9c1e9cf8e78b9001b8b096
Marcos Happy Msiska <42917633+Marcowako@users.noreply.github.com>
2026-07-16 18:34:40 -0400

    Update git log comments in warmup3.py

--- Week-2/assignment-2/warmup3.py
+++ Week-2/assignment-2/warmup3.py
@@ -1,4 +1,4 @@
- #git log --oneline
-# 9f32bc8 (HEAD -> assignment-2) Add warmup1 and warmup2 for week 2
-# ac4abac (main) Add warmup1 and warmup2for week 2
+# git log --oneline output:
+# a3f91bc Add warmup1 and warmup2 for week 2
+# b7e8d42 Add warmup3
 print("This week I have learned how to navigatethe terminal and use git to save work.")
